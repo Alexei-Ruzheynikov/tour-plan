b25819907690d53a4c58501a468ef2df199c062c
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-05-22 14:40:29 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -37,3 +37,11 @@ function init() {
     zoom: 14,
   });
 }
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
